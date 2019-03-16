cf7fd87daf73c79727c8a461fca4d2e3d1732747
user <you@example.com>
2019-03-16 11:58:15 +0100

    latest push from windows

--- source/index.js
+++ source/index.js
@@ -1,17 +1,14 @@
+exports.handler = async (event) => {
 
-// Note : cette fonction Lambda a une API Gateway comme trigger abec le mode 'Lambda proxy integration'
-
-exports.handler = (event, context, callback) => {
+	//var param = JSON.stringify(event.queryStringParameters.param);
+	var param = event.queryStringParameters.param;
+	if (param == 'fail') {
+      failure;
+    }
     
-	//Exemple de récupération de paramètres s'ils étaient transmis dans l'URL :
-	//JSON.parse(event.body).parametre) 
-	// Simulation d'erreur
-	//var erreur = 1/0;
-	//console.log("body : " + event.body);
-		
-	// Recuperation du message envoyé en JSON :
-	var data = JSON.stringify(event);
-	var val1 = require('querystring').parse(event.val1);
-	console.log("val1: " + val1);
-	console.log("request: " + "coucou");
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify(param),
+    };
+    return response;
 };
